5b641487a6f223a2ed5f77c4de27ab24c08fa7f3
Debbie Nichols <dnichols346@gmail.com>
2022-09-15 12:35:07 -0700

    Initial commit

---
+++ src/components/CardComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <p>Card 1</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardComp',
+  props: {
+    msg: String,
+  },
+});
+</script>
