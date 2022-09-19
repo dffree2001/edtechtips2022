ffed567672054253ee7393b2728db9f39176d314
Debbie Nichols <dnichols346@gmail.com>
2022-09-19 16:03:34 -0700

    Cards: Added data, Completed style sizing.

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="card mx-auto mx-1 my-2" style="width: 100%">
+  <div class="card mx-auto mx-1 h-100" style="width: 100%;">
     <div class="card-header">{{ header }}</div>
     <img src="" alt="" class="card-img-top">
     <div class="card-body" >
-      <p>{{ msg }}</p>
+      <ul>
+        <li v-if="msg1" style="text-align: left">{{ msg1 }}</li>
+        <li v-if="msg2" style="text-align: left">{{ msg2 }}</li>
+        <li v-if="msg3" style="text-align: left">{{ msg3 }}</li>
+        <li v-if="msg4" style="text-align: left">{{ msg4 }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -14,14 +19,17 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'CardComp',
   props: {
-    msg: String,
     header: String,
+    msg1: String,
+    msg2: String,
+    msg3: String,
+    msg4: String,
   },
 });
 </script>
 
 <style scoped>
  .card {
-    width: 18rem;
+    width: 100%;
  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home container-fluid">
-    <div class="row">
-      <div class="col-md-6">
-        <card-comp msg="My message" header="Getting Started" />
+    <div class="row card-deck" style="display:flex; align-items: stretch;" >
+      <div class="col-md-6 my-2"  >
+        <card-comp header="Getting Started" :msg1='c1l1' :msg2='c1l2' :msg3='c1l3' :msg4='c1l4' />
       </div>
-     <div class="col-md-6">
-        <card-comp msg="My message" header="Getting Started" />
+      <div class="col-md-6 my-2" >
+        <card-comp header="Homeschool Help" :msg1='c2l1' :msg2="c2l2" :msg3='c2l3' :msg4='c2l4' />
       </div>
     </div>
   </div>
@@ -19,6 +19,17 @@ export default defineComponent({
   name: 'HomeView',
   components: {
     CardComp,
+  },
+  data() {
+    return {
+      c1l1: 'How do I get started?',
+      c1l2: 'What are the requirements?',
+      c1l3: 'Can someone else homeschool my child?',
+      c1l4: 'State homeschool laws.',
+      c2l1: 'Line1',
+      c2l2: 'Line2',
+      c2l3: 'Line3',
+    };
   },
 });
 </script>
