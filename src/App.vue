8250c8d44584453746b8084b610e69c4e6723711
Debbie Nichols <dnichols346@gmail.com>
2022-09-22 13:41:39 -0700

    Added new Getting Started page.

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     </header>
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/starting">Getting Started</router-link>
     </nav>
     <router-view />
     <footer>
---
+++ src/views/GettingStarted.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Getting Started</h1>
+  <p>Info</p>
+</template>
+
+<script>
+export default {
+    name: 'GettingStartedView'
+}
+</script>
+
+<style>
+
+</style>
